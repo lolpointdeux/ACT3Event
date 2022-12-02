3a21c6c0e11126e3e23b0b386e5efb70eaf80b07
LOL.2 <180150@site.asty-moulin.be>
2022-12-02 11:40:41 +0100

    Ajoutez des fichiers projet.

---
+++ ACT3Event/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT3Event
+{
+    internal class Class1
+    {
+        public void trimone(int a, int b, int c, out string type)
+        {
+            type = null;
+            double d;
+            double x1;
+            double x2;
+
+            d = System.Math.Pow(b, 2) - 4 * a * c;
+            if (d >= 0)
+            {
+                if (d > 0)
+                {
+                    x1 = (-b + System.Math.Sqrt(d) / (-2 * a));
+                    x2 = (-b - System.Math.Sqrt(d) / (-2 * a));
+                    type = "Il y a deux racines réelles :" + x1 + " et " + x2;
+                }
+                else
+                {
+                    x1 = (-b) / (2 * a);
+                    type = "Il y a une seule racine : " + x1;
+                }
+            }
+            else
+            {
+                type = "Il n'y a pas de racine";
+            }
+        }
+    }
+}
---
+++ ACT3Event/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ACT3Event
+{
+    
+    public partial class MainWindow : Window
+    {
+        int[] Numbers = new int[3];
+        DispatcherTimer timer = new DispatcherTimer();
+        Class1 class1 = new Class1();
+        string type = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            A.PreviewTextInput += new TextCompositionEventHandler(VTI);
+            B.PreviewTextInput += new TextCompositionEventHandler(VTI);
+            C.PreviewTextInput += new TextCompositionEventHandler(VTI);
+
+            calc.MouseEnter += new MouseEventHandler(srvVBtn);
+            calc.MouseLeave += new MouseEventHandler(stopSrvVBtn);
+
+
+        }
+
+        private void Calculer(object sender, MouseButtonEventArgs e)
+        {
+            class1.trimone(int.Parse(A.Text), int.Parse(B.Text), int.Parse(C.Text), out type);
+            Window1 secondpage = new Window1();
+            secondpage.txtResultat.Text = type;
+            Visibility = Visibility.Hidden;
+        }
+
+        private void VTI(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text != "," && !EstEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+            if (((TextBox)sender).Text.IndexOf(e.Text) > -1)
+            {
+                e.Handled = true;
+            }
+            else
+            {
+
+
+            }
+        }
+
+        private bool EstEntier(string userText)
+        {
+            return int.TryParse(userText, out _);
+        }
+
+        private void srvVBtn(object sender, EventArgs e)
+        {
+            virtualBtn.Visibility = Visibility.Visible;
+            virtualBtn.Background = Brushes.Red;
+        }
+
+        private void stopSrvVBtn(object sender, EventArgs e)
+        {
+            virtualBtn.Visibility = Visibility.Hidden;
+        }
+
+    }
+}
